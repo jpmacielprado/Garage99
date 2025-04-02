867eaf678fb188d83e5cacc782f11e10021766f7
jpmacielprado <jp.macielprado@gmail.com>
2025-04-02 08:37:50 -0300

    Começo do projeto Garage99; Projeto importado dentro IDE; EndPoint definido para /Garage; Teste da exibição do SQL ok.

---
+++ src/main/java/local/jotape/garage99/entities/Garage.java
@@ -0,0 +1,106 @@
+package local.jotape.garage99.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "garage")
+
+public class Garage {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private int ano;
+    private double km;
+    private String linkimagem;
+    
+    @Column(name = "descricaocompleta")
+    private String descricao;
+    
+    @Column(name = "avarias")
+    private String avarias;
+    
+    
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getKm() {
+        return km;
+    }
+
+    public void setKm(double km) {
+        this.km = km;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getAvarias() {
+        return avarias;
+    }
+
+    public void setAvarias(String avarias) {
+        this.avarias = avarias;
+    }
+
+    public String getLinkimagem() {
+        return linkimagem;
+    }
+
+    public void setLinkimagem(String linkimagem) {
+        this.linkimagem = linkimagem;
+    }
+  
+}
+
