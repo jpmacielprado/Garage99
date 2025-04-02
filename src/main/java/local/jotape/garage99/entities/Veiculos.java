6fcc1d1acf3f9d8f212a56cf772b16e70bec228a
jpmacielprado <jp.macielprado@gmail.com>
2025-04-02 09:25:29 -0300

    Garage99 1° -Importação/configuração do preojeto na IDE; >> -EndPoint definido como /Garage; >> -Teste da exibição SQL ok.

--- src/main/java/local/jotape/garage99/entities/Veiculos.java
+++ src/main/java/local/jotape/garage99/entities/Veiculos.java
@@ -8,9 +8,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "garage")
+@Table(name = "veiculos")
 
-public class Garage {
+public class Veiculos {
+    
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
